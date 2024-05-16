b145b4bafe16dadc2c8361552733dbcd340262e7
xguillau <guillaume.xu@ynov.com>
2024-05-16 16:27:38 +0200

    corrige erreur de site non responsive

--- src/js/function.js
+++ src/js/function.js
@@ -1,15 +1,48 @@
 var Password = document.getElementById("UserPasswordInput");
 var Confirm_Password = document.getElementById("Confirm-PasswordInput");
+var Email = document.getElementById("UserMailInput");
+var Username = document.getElementById("UserInput");
 
 // Verify if Password is the same as Confirm Password
-function PasswordIsEqual(){
-    if(Password.value != Confirm_Password.value){
-        Confirm_Password.setCustomValidity("Password don't match");
-    }else{
-        Confirm_Password.setCustomValidity('');
-    }
+function PasswordIsEqual() {
+  if (Password.value != Confirm_Password.value) {
+    Confirm_Password.setCustomValidity("Password don't match");
+  } else {
+    Confirm_Password.setCustomValidity("");
+  }
 }
 
 Password.onchange = PasswordIsEqual;
 Confirm_Password.onkeyup = PasswordIsEqual;
 
+function IsValid(name) {
+  if (name.checkValidity()) {
+    name.classList.add("InputField");
+  } else {
+    name.classList.remove("InputField");
+  }
+}
+
+Password.onchange = function () {
+  IsValid(Password);
+};
+Confirm_Password.onchange = function () {
+  IsValid(Confirm_Password);
+};
+Email.onchange = function () {
+  IsValid(Email);
+};
+Username.onchange = function () {
+  IsValid(Username);
+};
+
+document.getElementById("myForm").addEventListener("submit", function (event) {
+  if (
+    !Password.checkValidity() ||
+    !Confirm_Password.checkValidity() ||
+    !Email.checkValidity() ||
+    !Username.checkValidity()
+  ) {
+    event.preventDefault();
+  }
+});
